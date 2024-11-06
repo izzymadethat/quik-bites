aeea35e209cdbc436e249adb84b663d43b5f0903
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-11-06 16:10:07 -0500

    feat(homepage): built restaurants display section suited for multiple categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import PromoCardGrid from "./components/PromoCardGrid";
+import RestaurantCard from "./components/RestaurantCard";
 
 function App() {
   const [getBack, setGetBack] = useState("");
@@ -20,6 +22,22 @@ function App() {
     <div className="container mx-auto">
       <Header />
       <Banner />
+      <PromoCardGrid />
+      <section className="my-10 space-y-2">
+        <h2 className="text-lg font-bold">Restaurants (Will be categorized)</h2>
+        <section className="grid grid-cols-4 gap-8">
+          {Array.from({ length: 4 }).map((rest, i) => (
+            <RestaurantCard
+              key={i}
+              title={`Restaurant ${i + 1}`}
+              rating={4.2}
+              deliveryFee={0}
+              fastestTime={35}
+              imgUrl={`https://placehold.co/400x300?text=Restaurant+${i}`}
+            />
+          ))}
+        </section>
+      </section>
     </div>
   );
 }
